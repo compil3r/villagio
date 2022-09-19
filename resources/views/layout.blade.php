c6d222a4cdcb1984a25295b25e3c709df8942a4a
Vitor Hugo Lopes <vtrhugolopes@gmail.com>
2022-09-19 00:28:25 -0300

    layout v 9

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -27,7 +27,7 @@
     <link href="css/owl.transitions.css" rel="stylesheet">
     <link href="css/on3step-style.css" rel="stylesheet">
     <link href="css/queries-on3step.css" media="all" rel="stylesheet" type="text/css">
-    <link href="css/app.css?v=8" rel="stylesheet">
+    <link href="css/app.css?v=9" rel="stylesheet">
 </head>
 
 <body class="choc">
